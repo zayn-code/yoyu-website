c1f07e9c8c9545e053b5913e17510309ba9a2502
zhangyao <972858472@qq.com>
2024-06-25 19:53:56 +0800

    update .gitignore

---
+++ product.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        title: '魅影浏览器',
+        alisa: 'meiying',
+        app_id: 'com.yueyou.meiyingbrowser',
+        desc: '魅影浏览器',
+        downloadUrl: 'https://h5coml.vivo.com.cn/h5coml/appdetail_h5/browser_v2/index.html?appId=3677282',
+    },
+    {
+        title: '秘咪播放器',
+        alisa: 'mimi',
+        app_id: 'com.yueyou.mimiplayer',
+        desc: '秘咪播放器',
+        downloadUrl: 'https://h5coml.vivo.com.cn/h5coml/appdetail_h5/browser_v2/index.html?appId=3681463',
+    }
+]
