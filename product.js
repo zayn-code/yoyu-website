74868bde236a64ca0e5a9e8467c308de5cf87b07
zhangyao <972858472@qq.com>
2024-06-26 11:34:15 +0800

    商品

--- product.js
+++ product.js
@@ -3,14 +3,14 @@ const products = [
         title: '魅影浏览器',
         alisa: 'meiying',
         app_id: 'com.yueyou.meiyingbrowser',
-        desc: '魅影浏览器',
-        downloadUrl: 'https://h5coml.vivo.com.cn/h5coml/appdetail_h5/browser_v2/index.html?appId=3677282',
+        desc: '魅影浏览器是一款专注于提供顺畅、安全、个性化的网络浏览体验的APP。它利用先进的浏览技术和用户友好的设计，使您在浏览网页、获取信息时享受无与伦比的便利。魅影浏览器的特色还包括：强大的隐私保护功能，能有效防止网络跟踪；高效的数据压缩技术，让您在节省流量的同时，依旧能享受快速的加载速度；以及丰富的扩展插件，让您可以根据自己的需求定制浏览器，提升浏览体验。',
+        downloadUrl: '',
     },
     {
         title: '秘咪播放器',
         alisa: 'mimi',
         app_id: 'com.yueyou.mimiplayer',
-        desc: '秘咪播放器',
-        downloadUrl: 'https://h5coml.vivo.com.cn/h5coml/appdetail_h5/browser_v2/index.html?appId=3681463',
+        desc: '秘咪播放器是一款功能强大，兼容性极高的多媒体播放APP。无论您想要播放的是音乐、电影、电视剧，还是直播节目，秘咪播放器都能为您提供流畅且高质量的播放体验。秘咪播放器支持各种流行的媒体格式，无需转码，即可直接播放。其独特的强化音频和视频处理技术，能让您在任何设备上都能享受到出色的视听效果。同时，秘咪播放器还拥有简洁明快的用户界面和丰富的个性化设置，让您的媒体播放更加便捷，更加个性化。',
+        downloadUrl: '',
     }
 ]
